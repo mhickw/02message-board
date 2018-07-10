75441591c7e1c72aa5f676ad9f7b2d80180d14ff
mi.isaka <mintcat19@hotmail.co.jp>
2018-07-10 21:31:29 +0900

    Aded getAllmesages

--- src/utils/DBUtil.java
+++ src/utils/DBUtil.java
@@ -4,8 +4,10 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+//データベースへ接続する部分やSQL文を実行する部分を担うクラス = DAO
+
 public class DBUtil {
-    private static final String PERSISTENCE_UNIT_NAME = "message_board";
+    private static final String PERSISTENCE_UNIT_NAME = "02message-board";
     private static EntityManagerFactory emf;
 
     public static EntityManager createEntityManager() {
